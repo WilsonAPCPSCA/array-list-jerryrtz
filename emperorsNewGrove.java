34d59bfad6d2ff273ffad47d2e2d4591e586be42
jerryrtz <32171513+jerryrtz@users.noreply.github.com>
2018-01-16 08:26:59 -0800

    Add files via upload

---
+++ emperorsNewGrove.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class emperorsNewGrove {
+	public static void main(String[] args) {
+		{
+ArrayList <String> emperorCast = new ArrayList<String>();
+emperorCast.add("Kuzco");
+emperorCast.add("Yzma");
+emperorCast.add("Chaca");
+emperorCast.add("Tipo");
+emperorCast.add("Kronk");
+emperorCast.add("Bucky");
+emperorCast.add("Pacha");
+}
+		}
+	}
